325757ae8d1be861e4c4b0879f468c8cdd944820
uchikosi yuuki <ucchikohiwithsugar@gmail.com>
2023-12-28 12:19:35 +0900

    ほぼ完成

--- src/Person.java
+++ src/Person.java
@@ -1,29 +1,34 @@
 
 public class Person {
+
 	public String name = null;
 	public int age = 0;
 
-	public Person( ){}
 //	コンストラクタ①
+	public Person( ){}
+
+//	コンストラクタ②
 	public Person(String name, int age){
 		this.name = name;
 		this.age = age;
 	}
-//	コンストラクタ②
 
-	public Person(String name){
-		this.name = name; this.age = 0;
-	}
 //	コンストラクタ③
-
-	public Person(int age){
-		this.name = "名前"; this.age = age;
+	public Person(String name){
+		this.name = name;
+		this.age = 0;
 	}
-//	コンストラクタ④
 
+//	コンストラクタ④
+	public Person(int age){
+		this.name = "名前なし";
+		this.age = age;
+	}
+
+//	コンストラクタ⑤
 	public Person(int age, String name){
 		this.name = name;
 		this.age = age;
 	}
-//	コンストラクタ⑤
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,31 @@
 
 public class Test {
 	public static void main(String[] args){
+//		コンストラクタ1を使ったインスタンス化
 		Person taro= new Person();
 		taro.name = "taro";
 		taro.age = 18;
 		System.out.println(taro.name);
 		System.out.println(taro.age);
-//		コンストラクタ1を使ったインスタンス化。
 
+//		コンストラクタ2を使ったインスタンス化
 		Person jiro = new Person("jiro", 20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
-//		コンストラクタ2を使ったインスタンス化
+
+//		コンストラクタ３を使ったインスタンス化
+		Person saburo = new Person("saburo");
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+
+//		コンストラクタ4を使ったインスタンス化
+		Person nasi = new Person("名前なし",25);
+		System.out.println(nasi.name);
+		System.out.println(nasi.age);
+
+//		コンストラクタ5を使ったインスタンス化
+		Person hanako = new Person("hanako",17);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
 	}
 }
